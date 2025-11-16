0317c0684e11c0bf77df1b0c73cd513bdfe44b8f
tothdomi22 <tothdomi22@gmail.com>
2025-11-16 21:49:31 +0100

    create empty user controller

--- src/main/java/com/dominik/barberbook/mappers/UserMapper.java
+++ src/main/java/com/dominik/barberbook/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.dominik.barberbook.mappers;
 import com.dominik.barberbook.dtos.admin.user.UserAdminUpdateRequest;
 import com.dominik.barberbook.dtos.admin.user.UserCreationRequest;
 import com.dominik.barberbook.dtos.user.UserDto;
+import com.dominik.barberbook.dtos.user.UserUpdateRequest;
 import com.dominik.barberbook.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -17,4 +18,6 @@ public interface UserMapper {
   User toEntity(UserCreationRequest request);
 
   void adminUpdate(UserAdminUpdateRequest request, @MappingTarget User user);
+
+  void userUpdate(UserUpdateRequest request, @MappingTarget User user);
 }
