a2dd079d316b3f19ac981501eadd5c9dbc841b2f
tothdomi22 <tothdomi22@gmail.com>
2025-11-16 14:21:59 +0100

    spring init

---
+++ src/main/java/com/dominik/barberbook/BarberbookApplication.java
@@ -0,0 +1,13 @@
+package com.dominik.barberbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominik/barberbook/BarberbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominik.barberbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
