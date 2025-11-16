1d9fd8f1b14212c95230ce0274fd440584e9df0d
tothdomi22 <tothdomi22@gmail.com>
2025-11-16 17:28:17 +0100

    added entities

---
+++ src/main/java/com/dominik/barberbook/entities/Appointment.java
@@ -0,0 +1,36 @@
+package com.dominik.barberbook.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents an appointment made by a user for a specific service type. Contains information about
+ * the reservation date and links to the user and service.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "appointments")
+public class Appointment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @NotNull
+  @Column(name = "reservation_date")
+  private Instant reservationDate;
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private com.dominik.barberbook.entities.User user;
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "service_type_id")
+  private com.dominik.barberbook.entities.ServiceType serviceType;
+}
