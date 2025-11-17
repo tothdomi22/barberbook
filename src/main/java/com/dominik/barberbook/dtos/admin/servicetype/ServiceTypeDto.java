7d55855d51572488abd3650c79b94873914518f1
tothdomi22 <tothdomi22@gmail.com>
2025-11-17 19:48:57 +0100

    appointment controller finalized

--- src/main/java/com/dominik/barberbook/dtos/admin/servicetype/ServiceTypeDto.java
+++ src/main/java/com/dominik/barberbook/dtos/admin/servicetype/ServiceTypeDto.java
@@ -1,8 +1,6 @@
 package com.dominik.barberbook.dtos.admin.servicetype;
 
-import com.dominik.barberbook.entities.Appointment;
 import java.time.LocalTime;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,5 +10,4 @@ public class ServiceTypeDto {
     private Integer id;
     private String type;
     private LocalTime length;
-    private Set<Appointment> appointments;
 }
